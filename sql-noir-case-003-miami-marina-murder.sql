ffabd67544e30ca7c90e1356ded821b52c2b3dbf
Tamara McG <tamaramcgiffin@gmail.com>
2025-10-24 11:50:57 -0700

    Still solving...

--- sql-noir-case-003-miami-marina-murder.sql
+++ sql-noir-case-003-miami-marina-murder.sql
@@ -2,3 +2,14 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+Case #003: The Miami Marina Murder
+A body was found floating near the docks of Coral Bay Marina in the early hours of August 14, 1986. Your job, detective, is to find the murderer and bring them to justice. This case might require the use of JOINs, wildcard searches, and logical deduction. Get to work, detective.
+
+Objectives
+1.
+Find the murderer. ( Start by finding the crime scene and go from there )
+
+SELECT * FROM crime_scene
+WHERE location = "Coral Bay Marina";
+
