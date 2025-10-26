dad88b27dcc73d5fa3e7e5f337d02eb0d46ced12
Tamara McG <tamaramcgiffin@gmail.com>
2025-10-26 12:06:42 -0700

    Document case #006: The Vanishing Diamond
    Opened case...

--- sql-noir-case-004-vanishing-diamond.sql
+++ sql-noir-case-004-vanishing-diamond.sql
@@ -2,3 +2,10 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+Case #006: The Vanishing Diamond
+At Miami’s prestigious Fontainebleau Hotel charity gala, the famous “Heart of Atlantis” diamond necklace suddenly disappeared from its display.
+
+Objectives
+1.
+Find who stole the diamond.
